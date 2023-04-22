7e7220eb20416f3e161892abc3209ece738c7358
Miguel Arriaga Velasco <marriagav3@gmail.com>
2023-04-22 15:49:16 -0600

    fixing warnings

--- src/hooks/useHttp.tsx
+++ src/hooks/useHttp.tsx
@@ -9,21 +9,25 @@ export const useHttp = (props: useHttpProps) => {
   const [isLoading, setIsLoading] = useState(false);
   const [fetchedData, setFetchedData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      const response = await fetch(props.url);
-      const data = await response.json();
-      setIsLoading(false);
-      setFetchedData(data);
-    };
-    console.log("Sending Http request to URL: " + props.url);
-    try {
-      fetchData();
-    } catch (e) {
-      console.error(e);
-    }
-  }, props.dependencies);
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        setIsLoading(true);
+        const response = await fetch(props.url);
+        const data = await response.json();
+        setIsLoading(false);
+        setFetchedData(data);
+      };
+      console.log("Sending Http request to URL: " + props.url);
+      try {
+        fetchData();
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [props.url, ...props.dependencies]
+  );
 
   return { isLoading, fetchedData };
 };
