c1d3d4bc75154dede3a41bb6dd6412698a75ff23
Miguel Arriaga Velasco <marriagav3@gmail.com>
2023-04-22 14:54:07 -0600

    firebase deploy and intitial setup

---
+++ src/services/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCR5N_kXQFjwy6V8hRjLN6tIEub11FwoI",
+  authDomain: "shopping-cart-ab943.firebaseapp.com",
+  projectId: "shopping-cart-ab943",
+  storageBucket: "shopping-cart-ab943.appspot.com",
+  messagingSenderId: "111258993793",
+  appId: "1:111258993793:web:c9270d1fcf73fe80dc7980",
+};
+
+const app = initializeApp(firebaseConfig);
